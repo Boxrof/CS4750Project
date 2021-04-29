090ac13368018b3878e0e07b1ace4195d9baaebb
Boxrof <vir.moh@gmail.com>
2021-04-28 23:38:46 -0400

    fixed small bug

--- Data_Generation/makeDataCsv.py
+++ Data_Generation/makeDataCsv.py
@@ -34,9 +34,9 @@ with open('restaurants.csv', 'w+', encoding="utf-8") as f:
 		dateOpened = str(month) + "/" + str(day) + "/" + str(year)
 
 		if h1 > h2: #r_address, r_name, r_rating, r_price, r_phone_number, closing_time, opening_time, date_opened
-			outputList.append([row[5].replace("\n"," "), row[0], rating, review, price, phoneNumber, str(h2) + ":00", str(h1) + ":00", dateOpened])
+			outputList.append([row[5].replace("\n"," ").replace(",", ""), row[0], rating, review, price, phoneNumber, str(h2) + ":00", str(h1) + ":00", dateOpened])
 		else:
-			outputList.append([row[5].replace("\n",""), row[0], rating, review, price, phoneNumber, str(h1) + ":00", str(h2) + ":00", dateOpened])
+			outputList.append([row[5].replace("\n"," ").replace(",", ""), row[0], rating, review, price, phoneNumber, str(h1) + ":00", str(h2) + ":00", dateOpened])
 
 	csvwriter = csv.writer(f, lineterminator = '\n')
 	csvwriter.writerows(outputList)
