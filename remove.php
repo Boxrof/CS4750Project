aac1982e1beef02d6ca94a2b3cdb5259a07fb8a4
Jason <jasonwatkins@outlook.com>
2021-05-03 16:31:07 -0400

    system for multiple orders and past orders

--- remove.php
+++ remove.php
@@ -22,10 +22,17 @@
         
         if ($check->rowCount() == 1) {
             //If our cart has only one item left, remove the order
+            $res = $pdo->prepare("DELETE FROM Included WHERE meal_ID=:id");
+            $res->bindParam(":id",$_GET['meal_ID']);
+            $res->execute();
+            $res->closeCursor();
+
             $del = $pdo->prepare("DELETE FROM Orders WHERE order_number=:order");
             $del->bindParam(":order",$_GET['order_number']);
             $del->execute();
             $del->closeCursor();
+
+            $_SESSION['curr_order'] = -1;
         } else {
             //Do not remove the order while there are still items in the cart
             $res = $pdo->prepare("DELETE FROM Included WHERE meal_ID=:id");
