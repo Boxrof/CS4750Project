f212008c2957756a5f72217f9b3412980ca619c1
Jason <jasonwatkins@outlook.com>
2021-05-03 22:39:11 -0400

    added stored procedure to index for customer

--- index.php
+++ index.php
@@ -128,8 +128,79 @@
                     $res->closeCursor();
 
                     echo "</table>";
-                }
-                else {
+                } else if ($_SESSION['user_type'] == 'customer') {
+                    $res = $pdo->query("CALL TOP_RES();");
+                    $result = $res->fetchAll();
+                    $res->closeCursor();
+
+                    echo "<h1 class='text-center'> List of Top Restaurants: </h1>";
+                    echo "<br>";
+                    echo "<table class='table table-condensed table-hover' style='table-layout: fixed; border-collapse:collapse;'>";
+                        echo "<thead>";
+                            echo "<tr>";
+                                echo "<th scope='col'> Detail </th>";
+                                echo "<th scope='col'>Favorite </th>";
+                                echo "<th scope='col'>Address </th>";
+                                echo "<th scope='col'>Restaurant Name </th>";
+                                echo "<th scope='col'>Rating </th>";
+                                echo "<th scope='col'>Price </th>";
+                                echo "<th scope='col'>Cuisine </th>";
+                            echo "</tr>";
+                        echo "</thead>";
+
+
+                        $user_ID = $_SESSION['user_ID'];
+                        $queryS = "SELECT DISTINCT(r_ID) FROM Saved WHERE user_ID='" . $user_ID . "';";
+                        // echo $queryS . "<br>";
+                        $temp_res = $pdo->query($queryS);
+                        // while ($row = $res->fetch()) {
+                        //     echo $row . "<br>";
+                        // }
+                        $r_ID_array = $temp_res->fetchAll();
+                        if ($r_ID_array == false) {
+                            $r_ID_array = [];
+                        }
+
+                        for ($i = 0; $i < sizeof($r_ID_array); $i++) {
+                            $r_ID_array[$i] = $r_ID_array[$i][0];
+                        }
+
+                        foreach ($r_ID_array as $x) {
+                            // echo $x . "<br>";
+                            // foreach ($x as $y) {
+                            //     echo $y . "<br>";
+                            // }
+                        }
+                        //echo $r_ID_array;
+                
+                        foreach ($result as $row) {
+                            $r_id = $row["r_ID"];
+                            $address = $row["r_address"];
+                            echo "<tr>";
+                                echo "<th scope='row'><a class='btn btn-outline-secondary btn-sm' href='menu.php?r_id=",$r_id,"&address=",$address,"'> Click here to view menu </a></th>";
+                                if (isset($_SESSION['user_ID'])) {
+                                    //$user_ID = $_SESSION['user_ID'];
+                                    //$temp_res = $pdo->query("SELECT * FROM Saved WHERE user_ID='" . $user_ID . "' AND r_ID='" . $r_id . "'");
+                                    //$temp_res->execute();
+                                    //if ($temp_res->rowCount() > 0) {
+                                    if (in_array($r_id, $r_ID_array)) {
+                                        echo "<td><a href='saveFavorite.php?user_ID=",$_SESSION['user_ID'],"&r_ID=",$r_id,"'><img src='star_full.png' style='width:25px; height:24.0625px'></td></a>";
+                                    } else {
+                                        echo "<td><a href='saveFavorite.php?user_ID=",$_SESSION['user_ID'],"&r_ID=",$r_id,"'><img src='star_empty.png' style='width:25px; height:24.0625px'></td></a>";
+                                    }
+                                } else {
+                                    echo ('<td>Login</td>');
+                                }
+                                echo('<td> ' .  $row["r_address"]. '</td>');
+                                echo('<td> ' .  $row["r_name"]. '</td>');
+                                echo('<td> ' .  $row["r_rating"]. '</td>');
+                                echo('<td> ' .  $row["r_price"]. '</td>');
+                                echo('<td> ' .  $row["r_cuisine"]. '</td>');
+                            echo "</tr>";    
+                        }
+                
+                    echo "</table>";
+                } else {
                     echo $_SESSION['user_type'];
                 }
             }
